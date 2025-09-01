5561f497d65fc40d5997642f553128627f66bbc4
Your Name <you@example.com>
2025-09-01 18:10:54 +0900

    ..

--- clipping/settings.py
+++ clipping/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'article.apps.ArticleConfig',
     'taggit',
 ]
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'clipping.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'), 
+        'HOST': 'db',    # or your server IP
+        'PORT': '5432',  
     }
 }
 
